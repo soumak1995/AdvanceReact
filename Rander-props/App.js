c5b142a86efe2b0090a1a2f4b443b430b83f40c0
unknown <Soumak Maji>
2021-04-11 11:33:45 +0530

    reneder_prop upadted

--- Rander-props/App.js
+++ Rander-props/App.js
@@ -1,13 +1,24 @@
 import React from "react"
-import Menu from "./Menu"
-import Favorite from "./Favorite"
+import DataFetcher from "./DataFetcher"
 
-function App() {
+function App() {    
     return (
         <div>
-            <Menu something="whatever" />
-            <hr />
-            <Favorite />
+            <DataFetcher url="https://swapi.co/api/people/1">
+                {/**
+                 * Part 2: Call the function the DataFetcher is expecting.
+                 * If should receive the data and the loading state, and return the JSX
+                 * that makes use of that info. If the data is still loading, display
+                 * "Loading..." in an h1 element, and once the data has loaded, just display 
+                 * the data with `<p>{JSON.stringify(data)}</p>`
+                 * 
+                 * Remember: With the render props pattern, you can use a custom prop
+                 * (typically called `render`), OR you can use `props.children`. Based
+                 * on what's already written here for you, you should be able to figure
+                 * out which of these we're using. (You may have to make changes to the
+                 * DataFetcher component based on what you see here.)
+                 */}
+            </DataFetcher>
         </div>
     )
 }
